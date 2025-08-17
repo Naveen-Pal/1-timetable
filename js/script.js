7c8eae664d1130b3574bd94b347813a64387996d
Naveen-Pal <paln76050@gmail.com>
2025-08-18 02:39:38 +0530

    handled invalid roll number in frontend

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 function getTimetable() {
     var rollNumber = document.getElementById("rollNumber").value.toUpperCase();
     
-    // Assuming the images are in the "img" folder
-    var imageUrl = "img/" + rollNumber + ".png";
-
-    // Redirect to the timetable image
-    window.location.href = imageUrl;
+    // Convert roll number to integer for comparison
+    var rollNum = parseInt(rollNumber);
+    
+    // Validate roll number range
+    if (rollNum >= 25110001 && rollNum <= 25110361) {
+        // Assuming the images are in the "img" folder
+        var imageUrl = "img/" + rollNumber + ".png";
+        
+        // Redirect to the timetable image
+        window.location.href = imageUrl;
+    } else {
+        alert("Invalid Roll Number! Please enter a roll number between 25110001 and 25110361.");
+    }
 }
 
